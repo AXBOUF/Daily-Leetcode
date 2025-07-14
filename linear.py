7058065b3b1def76a477e9d5bdf764abce6e4309
Munal <165521222+AXBOUF@users.noreply.github.com>
2025-07-15 07:34:21 +1000

    Add files via upload

---
+++ linear.py
@@ -0,0 +1,8 @@
+#coding on hand skills
+#taking a dataset from sklearn.datasets (like Boston housing)
+import numpy as np
+from sklearn.datasets import load_boston
+import pandas as pd
+
+df = pd.DataFrame(load_boston().data, columns=load_boston().feature_names)
+print(df.head())
