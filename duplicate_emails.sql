b7da079748d7f32304011ccbfda2867148a2826a
Munal <165521222+AXBOUF@users.noreply.github.com>
2025-07-17 23:51:50 +1000

    Create 182. duplicate_emails.sql

---
+++ duplicate_emails.sql
@@ -0,0 +1,27 @@
+/*
+
+Example 1:
+
+Input: 
+Person table:
++----+---------+
+| id | email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+Output: 
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+Explanation: a@b.com is repeated two times.
+*/
+
+
+SELECT email ---it returns email only 
+FROM Person
+GROUP BY Email --- as column name Email
+HAVING COUNT(email) > 1; --- given condition if more than 1
