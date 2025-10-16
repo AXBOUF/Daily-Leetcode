126c5c059575d410a1f2250c349500f36bfb0e2f
Munal <165521222+AXBOUF@users.noreply.github.com>
2025-10-16 23:31:54 +1100

    Add function to validate parentheses in a string
    Implement isValid function to check valid parentheses.

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -21,7 +21,23 @@ def isValid(s):
     else:
         return False
 
+def isValid(s: str) -> bool:
+        pairs = {'(':')', '{':'}', '[':']'}
+        stack = []
 
+        for i, v in enumerate(s):
+
+            # Add to the top of the stack if it's a opening bracket
+            if v in '({[': stack += [v]
+
+            # If the stack is not empty and the top is the pair of the current closing bracket, pop it
+            elif stack and pairs[stack[-1]] == v: del stack[-1]
+
+            # If the stack became empty at any point ot the brackets didnt match, return False
+            else: return False
+
+        # Return true if the stack is empty
+        return not stack
 #keeping updated having hard time to make it simpe style
 
 
